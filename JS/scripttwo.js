a1a83c7daca6e742526eb2a40ba9dfac4b67ee0a
Bruno Michelan <brunoguima2000@gmail.com>
2020-08-28 19:09:57 -0300

    commit

--- JS/scripttwo.js
+++ JS/scripttwo.js
@@ -53,7 +53,7 @@ function searchFunction2() {
 
 function pblank() {
     labelV2 = document.getElementById('labelV1');
-    if (labelV2.textContent == "14"){
+    if (labelV2.textContent == "15"){
         window.location.href = "/pesquisar.html";
     }
 }
